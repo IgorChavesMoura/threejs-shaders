7064c37d0781194d9504f8c3eaecf4e989278dd3
Igor Moura <moura@DESKTOP-KSK7G10.localdomain>
2022-11-24 03:43:28 +0000

    Shader from HTML script tag

--- main.js
+++ main.js
@@ -8,22 +8,7 @@ const uniforms = {
     color: { value: new THREE.Vector3(1, 0, 0) }
 };
 
-const fragmentShader = `
-    #include <common>
-    
-    uniform vec3 color;
-    
-    void mainImage( out vec4 fragColor, in vec2 fragCoord )
-    {
-
-        // Output to screen
-        fragColor = vec4(color.x, color.y, color.z,1.0);
-    }
-    
-    void main() {
-        mainImage(gl_FragColor, gl_FragCoord.xy);
-    }
-`;
+const fragmentShader = document.getElementById('basic-color-shader').text;
 
 const fov = 75;
 const aspect = 2;
